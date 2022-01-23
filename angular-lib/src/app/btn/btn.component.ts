cd39266b27f40238bf524fab6350e3b0250c7a8e
Pradeep Saini <psaini@ex2india.com>
2022-01-23 19:19:25 +0530

    adding

---
+++ angular-lib/src/app/btn/btn.component.ts
@@ -0,0 +1,51 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-btn',
+  templateUrl: './btn.component.html',
+  styleUrls: ['./btn.component.scss']
+})
+export class BtnComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+/**
+   * Is this the principal call to action on the page?
+   */
+ @Input()
+ primary = false;
+
+ /**
+  * What background color to use
+  */
+ @Input()
+ backgroundColor?: string;
+
+ /**
+  * How large should the button be?
+  */
+ @Input()
+ size: 'small' | 'medium' | 'large' = 'medium';
+
+ /**
+  * Button contents
+  *
+  * @required
+  */
+ @Input()
+ label = 'Button';
+
+ /**
+  * Optional click handler
+  */
+ @Output()
+ onClick = new EventEmitter<Event>();
+
+ public get classes(): string[] {
+   const mode = this.primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+
+   return ['storybook-button', `storybook-button--${this.size}`, mode];
+ }
+}
